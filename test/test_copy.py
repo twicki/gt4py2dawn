52a93f98efb72e0623b866e3c9a5cb136dbf3f07
Tobias Wicky <tobiasw@vulcan.com>
2020-04-02 17:02:51 +0200

    switch to x86 backend

--- test/test_copy.py
+++ test/test_copy.py
@@ -15,9 +15,10 @@ def copy_stencil(in_field: gtscript.Field[float], out_field: gtscript.Field[floa
 def test_print():
     print(copy_stencil)
 
+
 def test_compile_and_run():
     shape = (10, 10, 10)
-    in_storage = gt4py.storage.ones(shape=shape, default_origin=(3, 3, 3), dtype=float, backend="gtmc")
-    out_storage = gt4py.storage.zeros(shape=shape, default_origin=(3, 3, 3), dtype=float, backend="gtmc")
+    in_storage = gt4py.storage.ones(shape=shape, default_origin=(3, 3, 3), dtype=float, backend="gtx86")
+    out_storage = gt4py.storage.zeros(shape=shape, default_origin=(3, 3, 3), dtype=float, backend="gtx86")
     copy_stencil(in_storage, out_storage)
     print(out_storage)
